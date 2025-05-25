eac6197d9b9994ea1d1b35bb9133d2f008fb9dae
Pray45 <prayvijaybhai10@gmail.com>
2025-05-25 15:35:15 +0530

    first commit

---
+++ src/lib/DB.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose"
+
+const uri = process.env.MONGODB_URI!
+if(!uri) throw new Error('please check "MONGODB_URI" in .emv file...!!!')
+
+let cache = global.mongoose
+if(!cache) cache = global.mongoose = {conn: null , promise: null}
+
+export const DataBaseConnection = async() => {
+    
+    if(cache.conn) return cache.conn
+
+    if(!cache.promise) cache.promise = mongoose.connect(uri).then(() => mongoose.connection)
+
+    try {
+
+        cache.conn = await cache.promise
+
+    } catch (error) {
+
+        cache.promise = null;
+        throw error
+
+    }
+
+    return cache.conn
+
+}
